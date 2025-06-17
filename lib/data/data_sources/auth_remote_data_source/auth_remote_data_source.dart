10f7da4ecc2fe7ab12a19ed1b82d0db6666d2fe5
mohamedmagdi6 <mohamedmagdii137@gmai.com>
2025-06-17 16:32:39 +0300

    implement AuthRemoteDataSource with signIn and signUp methods

--- lib/data/data_sources/auth_remote_data_source/auth_remote_data_source.dart
+++ lib/data/data_sources/auth_remote_data_source/auth_remote_data_source.dart
@@ -11,5 +11,4 @@ abstract class AuthRemoteDataSource {
     String email,
     String password,
   );
-  Future<void> signOut();
 }
---
+++ lib/data/data_sources/auth_remote_data_source/auth_remote_data_source_impl.dart
@@ -0,0 +1,41 @@
+import 'package:auvent_flutter_task/core/failures/failures.dart';
+import 'package:auvent_flutter_task/core/failures/firebase_error_mapper.dart';
+import 'package:auvent_flutter_task/data/data_sources/auth_remote_data_source/auth_remote_data_source.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final FirebaseAuth _firebaseAuth;
+  AuthRemoteDataSourceImpl(this._firebaseAuth);
+  @override
+  Future<Either<Failure, UserCredential>> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      final userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return Right(userCredential);
+    } on FirebaseAuthException catch (e) {
+      return Left(FirebaseErrorMapper.mapFirebaseAuthException(e));
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserCredential>> signUpWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      final userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return Right(userCredential);
+    } on FirebaseAuthException catch (e) {
+      return Left(FirebaseErrorMapper.mapFirebaseAuthException(e));
+    }
+  }
+}
